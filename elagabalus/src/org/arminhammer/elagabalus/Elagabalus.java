424902f3849089fac18c2e4e3f0a4f9b95f8e513
arminhammer <arminhammer@gmail.com>
2013-07-30 19:51:53 -0400

    added initializeFile()

--- elagabalus/src/org/arminhammer/elagabalus/Elagabalus.java
+++ elagabalus/src/org/arminhammer/elagabalus/Elagabalus.java
@@ -3,6 +3,8 @@
  */
 package org.arminhammer.elagabalus;
 
+import java.io.File;
+
 /**
  * @author armin
  *
@@ -19,11 +21,13 @@ public class Elagabalus {
 		this.autoPersist = false;
 		this.fileSize = 100;
 		this.filePath = filePath;
+		this.initializeFile();
 	}
 	
 	public Elagabalus(String filePath, long fileSize) {
 		this.fileSize = fileSize;
 		this.filePath = filePath;
+		this.initializeFile();
 	}
 	
 	public int[] write(String id, byte[] data) {
@@ -36,6 +40,54 @@ public class Elagabalus {
 	
 	public void save() {
 		
+	}
+	
+	private void initializeFile() {
+		/* Check to see if the file exists.  Verify that it is a valid
+         * PojoStick object store, and then do some further validations
+         * to make sure that it is useable.  If it doesn't exist, create
+         * it.
+         */
+		File testFile = new File(this.filePath);
+        if (this.filePath.exists()) {
+            try {
+                this.verifyFile();
+            } catch (FileNotVerifiableException e) {
+                LOGGER.error("File " + e + " exists, but was not verifiable as a valid PojoStick file.");
+            }
+            if (this.pojofile.isDirectory()) {
+                LOGGER.error("File is a directory and cannot be used.");
+                System.exit(0);
+            }
+            if (!this.pojofile.canRead() || !this.pojofile.canWrite()) {
+                LOGGER.error("PojoStick does not have full permissions to use this file.");
+                System.exit(0);
+            }
+        } else {
+            try {
+                File parent = this.pojofile.getParentFile();
+                //System.out.println("Parent: " + parent);
+                if (!this.pojofile.getParentFile().exists()) {
+                    this.pojofile.getParentFile().mkdirs();
+                }
+                FileWriter writer;
+                try {
+                    writer = new FileWriter(pojofile, true);
+                    writer.write(queueSeparator + "\n");
+                    writer.close();
+                } catch (IOException ex) {
+                    LOGGER.error("IOError: " + ex);
+                }
+                //System.out.println("pojofile is " + this.pojofile);
+                boolean created = this.pojofile.createNewFile();
+                //System.out.println(created);
+
+            } catch (IOException ex) {
+                LOGGER.error("IOException " + ex);
+            }
+        }
+        queueThread = new QueueProcessor(this);
+    }
 	}
 	
 }
