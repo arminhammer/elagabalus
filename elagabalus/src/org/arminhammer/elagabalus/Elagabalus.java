129d9f30297fbac85641e9ce26d72acc25cef896
arminhammer <arminhammer@gmail.com>
2013-07-30 19:38:57 -0400

    Added public functions

--- elagabalus/src/org/arminhammer/elagabalus/Elagabalus.java
+++ elagabalus/src/org/arminhammer/elagabalus/Elagabalus.java
@@ -9,4 +9,33 @@ package org.arminhammer.elagabalus;
  */
 public class Elagabalus {
 
+	private final static long MAX_FILE_SIZE = 4294967296;
+	
+	private boolean autoPersist;
+	private long fileSize;
+	private String filePath;
+	
+	public Elagabalus(String filePath) {		
+		this.autoPersist = false;
+		this.fileSize = 100;
+		this.filePath = filePath;
+	}
+	
+	public Elagabalus(String filePath, long fileSize) {
+		this.fileSize = fileSize;
+		this.filePath = filePath;
+	}
+	
+	public int[] write(String id, byte[] data) {
+		return null;
+	}
+	
+	public byte[] read(String id) {
+		return null;
+	}
+	
+	public void save() {
+		
+	}
+	
 }
